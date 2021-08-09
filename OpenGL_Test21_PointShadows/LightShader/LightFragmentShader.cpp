77313abf3aca97b5b0e119a78d6e6f5b842706b5
LiangHaoXiang <1013568397@qq.com>
2021-08-09 20:44:15 +0800

    优化PCF算法，减少采样次数

--- OpenGL_Test21_PointShadows/LightShader/LightFragmentShader.cpp
+++ OpenGL_Test21_PointShadows/LightShader/LightFragmentShader.cpp
@@ -15,6 +15,15 @@ uniform vec3 viewPos;
 
 uniform float far_plane;
 
+vec3 sampleOffsetDirections[20] = vec3[]
+(
+   vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1),
+   vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
+   vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
+   vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
+   vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
+);
+
 float ShadowCalculation(vec3 fragPos)
 {
     vec3 fragToLight = fragPos - lightPos;
@@ -25,25 +34,18 @@ float ShadowCalculation(vec3 fragPos)
     float currentDepth = length(fragToLight);
     // 检查当前片段是否在阴影中
     float shadow = 0.0;
-    float bias = 0.05;
-    float samples = 4.0;
-    float offset = 0.1;
-    for (float x = -offset; x < offset; x += offset / (samples * 0.5))
+    float bias = 0.15;
+    int samples = 20;
+    float viewDistance = length(viewPos - fragPos);
+    float diskRadius = 0.05;
+    for(int i = 0; i < samples; ++i)
     {
-        for(float y = -offset; y < offset; y += offset / (samples * 0.5))
-        {
-            for(float z = -offset; z < offset; z += offset / (samples * 0.5))
-            {
-                float closestDepth = texture(depthMap, fragToLight + vec3(x, y, z)).r;
-                closestDepth *= far_plane;   // Undo mapping [0;1]
-                if(currentDepth - bias > closestDepth)
-                {
-                    shadow += 1.0;
-                }
-            }
-        }
+        float closestDepth = texture(depthMap, fragToLight + sampleOffsetDirections[i] * diskRadius).r;
+        closestDepth *= far_plane;   // Undo mapping [0;1]
+        if(currentDepth - bias > closestDepth)
+            shadow += 1.0;
     }
-    shadow /= (samples * samples * samples);
+    shadow /= float(samples);
     return shadow;
 }
 
